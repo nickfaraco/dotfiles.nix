f45685e4d8af123f4a5760afbb30cbb83ced2b61
Niccolò Faraco <faraco.nic@gmail.com>
2024-10-30 13:28:04 +0100

    Added Homebrew

--- flake.nix
+++ flake.nix
@@ -29,6 +29,18 @@
 
     # Stylix
     stylix.url = "github:danth/stylix";
+
+    # Homebrew
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    # Optional: Declarative tap management
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
   };
 
   outputs = inputs @ {
@@ -37,6 +49,7 @@
     nixpkgs,
     home-manager,
     nur,
+    nix-homebrew,
     ...
   }: let
     #globalConfigs = {
@@ -78,6 +91,26 @@
 
           home-manager.extraSpecialArgs = {
             inherit nur inputs;
+          };
+        }
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            # Install Homebrew under the default prefix
+            enable = true;
+            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+            enableRosetta = true;
+            # User owning the Homebrew prefix
+            user = "nick";
+            # Optional: Declarative tap management
+            taps = {
+              "homebrew/homebrew-core" = inputs.homebrew-core;
+              "homebrew/homebrew-cask" = inputs.homebrew-cask;
+            };
+            # Optional: Enable fully-declarative tap management
+            #
+            # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+            mutableTaps = false;
           };
         }
       ];
